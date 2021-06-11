277b3d49e545f757b84a3e5d52eac03d6d797d76
chumples <84584789+chumples@users.noreply.github.com>
2021-06-11 09:26:29 +0700

    removed old code, still can't get grid to size correctly :(

--- scripts.js
+++ scripts.js
@@ -57,25 +57,3 @@ function clearGrid() {
         container.removeChild(element);
     });
 }
-
-
-
-/*cell.addEventListener('mouseover',
-e => e.target.ClassList.add('my-color-class'))*/
-
-/*cell.addEventListener('mouseover', 
-e => e.target.classList.add('my-colour-class')
-)*/
-
-/*function makeGrid () {
-    for (let i= 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.className = 'row';
-    for (let j = 0; j < 16; j++) {
-        let box = document.createElement('div');
-        box.className = 'box';
-        row.appendChild(box);
-    }
-    document.querySelector('#container').appendChild(row);
-    }
-}*/
